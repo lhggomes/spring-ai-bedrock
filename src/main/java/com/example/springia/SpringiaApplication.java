710b8e09c3bd90719f2b321d8779cb35d1d4ad30
Lucas Gomes <lucas.henrique.s.go@gmail.com>
2024-06-04 21:35:51 -0300

    First Commit

---
+++ src/main/java/com/example/springia/SpringiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springia/SpringiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
