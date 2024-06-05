c700947f7308430a5a4e59086e2c100454aaf3a4
Lucas Gomes <lucas.henrique.s.go@gmail.com>
2024-06-04 21:53:54 -0300

    Fixing implementation

--- src/main/java/com/example/springai/SpringiaApplication.java
+++ src/main/java/com/example/springai/SpringiaApplication.java
@@ -1,4 +1,4 @@
-package com.example.springia;
+package com.example.springai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/springai/SpringiaApplicationTests.java
+++ src/test/java/com/example/springai/SpringiaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springia;
+package com.example.springai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
